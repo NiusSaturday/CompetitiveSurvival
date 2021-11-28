5ff93f36d567cf5ea03d27112a8731d0ae0c288b
lulu2002 <lose9749@gmail.com>
2021-11-28 23:39:54 +0800

    Initial commit

---
+++ src/main/kotlin/me/lulu/competitivesurvival/CompetitiveSurvival.kt
@@ -0,0 +1,19 @@
+package me.lulu.competitivesurvival
+
+import br.com.devsrsouza.kotlinbukkitapi.architecture.KotlinPlugin
+import br.com.devsrsouza.kotlinbukkitapi.dsl.command.arguments.player
+import br.com.devsrsouza.kotlinbukkitapi.dsl.command.command
+import br.com.devsrsouza.kotlinbukkitapi.dsl.command.sendSubCommandsList
+import br.com.devsrsouza.kotlinbukkitapi.dsl.command.simpleCommand
+import br.com.devsrsouza.kotlinbukkitapi.extensions.text.msg
+import br.com.devsrsouza.kotlinbukkitapi.extensions.text.plus
+import br.com.devsrsouza.kotlinbukkitapi.extensions.text.textOf
+import org.bukkit.ChatColor
+
+class CompetitiveSurvival : KotlinPlugin() {
+
+    override fun onPluginEnable() {
+
+    }
+
+}
